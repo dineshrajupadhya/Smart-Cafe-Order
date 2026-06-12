0bd7f613beb04125d365d1d7f728173303dceb5c
dineshrajupadhya <dineshrajupadhya86@gmail.com>
2026-06-12 16:40:17 +0530

    Fix: email working - update to new backend URL + clean emailService.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://smart-cafe-order-backend.onrender.com/api'
+  baseURL: process.env.REACT_APP_API_URL || 'https://smart-cafe-backend-2ibh.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
